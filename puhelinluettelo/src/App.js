c45951c6e80ef3bbbd425f455d44a384d8664a86
anonOstrich <jesper.kuutti@helsinki.fi>
2019-01-17 21:16:55 +0200

    Tehty -> 1.5]

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -2,11 +2,15 @@ import React, {useState} from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '032374328'}
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
     ]); 
 
     const [ newName, setNewName ] = useState('');
     const [ newNumber, setNewNumber] = useState(''); 
+    const [ filterString, setFilterString ] = useState(''); 
 
     const addNewInformation = (event) => {
         event.preventDefault(); 
@@ -27,11 +31,17 @@ const App = () => {
         setNewName(event.target.value); 
     }
 
-    const contactInformations = () =>  persons.map(p => <div key={p.name}>{`${p.name} ${p.number}`}</div>)
+    const contactInformations = () =>  {
+        const potentials = (filterString === '') ?
+         persons 
+         : persons.filter(p => p.name.toLowerCase().includes(filterString.toLowerCase()));
+        return  potentials.map(p => <div key={p.name}>{`${p.name} ${p.number}`}</div>)
+    }
 
     return (
     <div>
         <h2>Puhelinluettelo</h2>
+        rajaa näytettäviä <input onChange={(event) => setFilterString(event.target.value)}/>
         <form>
             <div>
                 nimi: <input value={newName} onChange={changeNameValue} />
